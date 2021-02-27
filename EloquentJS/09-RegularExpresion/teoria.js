d0e3546ca9d1a3e0fd73016fb09e33ed91bcac63
galuf <alex.flores.m@uni.pe>
2021-02-26 23:35:12 -0500

    RegEx final

--- EloquentJS/09-RegularExpresion/teoria.js
+++ EloquentJS/09-RegularExpresion/teoria.js
@@ -225,3 +225,221 @@ console.log(animalCount.test("15 pigs"));
 // > True
 console.log(animalCount.test("15 pigChikens"));
 // > False
+
+/*********** Replace methods**************/
+
+// los strings tienen el metodo replace, el cual reemplaza caracteres de la cadena por algo q se le indique
+let cadena = "papa";
+let newCadena = cadena.replace("a", "u");
+console.log(cadena);
+// > papa -> recordar que los strings son datos primitivos e inmutables
+console.log(newCadena);
+// > el replace retornara newCadena
+
+// Podemos buscar la cecuencia a reemplazar mediante una expresion regular
+console.log("Borobudur".replace(/[ou]/, "a")); // Cuando encuentre una 'o' o una 'u' cambiarla por 'a'
+// > Barobudur
+// Nos damos cuenta de que solo el primer caracter encontrado se reemplazo
+// Para buscar todas las matchs en la cadena usaremos 'g' al final de la expresion regular
+console.log("Borobudur".replace(/[ou]/g, "a")); // Cuando encuentre una 'o' o una 'u' cambiarla por 'a'
+// > Barabadar
+
+/********* Ventajas de usar replace method con RegEx **********/
+
+// Supongamos que tenemos una cadena de la forma
+// FirstName, LastName -> Lo que queremos hacer es cambiar esto a :
+// LastName FirstName -> Como lo hariamo ?
+
+let formatoInicial = "Barbara, Liskov\nTony, Flores\nGiomar, Vilca";
+console.log(formatoInicial.replace(/(\w+), (\w+)/g, "$2 $1"));
+// la expresion regular solicita palabra1, palabra2
+// palabra 1 se almacena en $1 y palabra2 en $2 -> hacen referencia a los subgrupos definidos con los parentesis
+// esto hace que modifiquemos la cadena
+// > Barabara Liskov
+// > Flores Tony
+// > Vilca Giomar
+
+// Es posible pasar como segundo argumento del replace a una funcion
+let s = "the cia and fbi";
+console.log(
+  s.replace(/\b(fbi|cia)\b/g, (string) => {
+    return string.toUpperCase();
+  })
+);
+
+// > the CIA and FBI
+// todos los elemetos match se pasan como parametro a la funcion y se operan
+
+// otro ejemplo de paso de parametro como funcion
+let stock = "1 limon, 2 cebollas y 101 huevos";
+function reducirUno(match, amount, unit) {
+  console.log(`match: ${match}, amount:${amount}, unit: ${unit}`);
+  amount = Number(amount) - 1;
+  if (amount == 1) {
+    unit = unit.slice(0, unit.lenght - 1);
+  } else if (amount == 0) {
+    amount = "no";
+  }
+  return `${amount} ${unit}`;
+}
+
+console.log(stock.replace(/(\d+) (\w+)/g, reducirUno));
+
+// > match: 1 limon, amount:1, unit: limon
+// > match: 2 cebollas, amount:2, unit: cebollas
+// > match: 101 huevos, amount:101, unit: huevos
+// > no limon, 1  y 100 huevos
+
+// Modifiquemos el programa de apellidos y nombres y hagamoslo con funciones
+// sea el string:
+
+let formatoInicial1 = "Barbara, Liskov\nTony, Flores\nGiomar, Vilca";
+function cambia(match, nombre, apellido) {
+  return `${apellido} ${nombre}`;
+}
+console.log(formatoInicial1.replace(/(\w+), (\w+)/g, cambia));
+// > Liskov Barbara
+// > Flores Tony
+// > Vilca Giomar
+
+// Tener en cuenta, los parametros que recive la funcion son (match, 1er Subgrupo, 2do Subgrupo, 3er Subgrupo, ...)
+
+/*********** Funcion que elimina comentarios ****************/
+// sabesmo que los comentarios de un lenguaje de programacion se escriben con
+// "//" -> y todo lo q le siga sera comentario
+// /* dentro */ -> todo lo dentro sera comentario
+function stripComentario(code) {
+  return code.replace(/\/\/.*|\/\*[^]*\*\//g, "");
+}
+// usamos [^] -> negacion de vacio, lo cual puede ser lo quesea
+// no usamos . en esa parte porq, aqui se puede incluir hasta saltos de linea
+// cosa q el punto (.) no acepta
+
+console.log(stripComentario("x = 1; // esto es un comentario"));
+// x = 1;
+console.log(stripComentario("x = 1; /*esto es un comentario*/"));
+// x = 1;
+console.log(stripComentario("x = 1; /*25555^^///+++*/"));
+// x = 1;
+console.log(stripComentario("1 /*x*/ + /*y*/ 1"));
+// 1  1
+// Esto es un error
+
+/***************    Greedy   *****************/
+
+// Esto se debe a que los operadores +, * , ?, son greddy
+// Esto quiere decir que tratan de abarcar lo mas posible de la cadena
+// para evitar este comportamiento pondemos ? despues del operador y
+// se cambia a nogreedy y busca los caracteres de menor tamaño posible
+function stripComentarioNoGredi(code) {
+  return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
+}
+console.log(stripComentarioNoGredi("1 /*x*/ + /*y*/ 1"));
+// 1 + 1 -> ahora si esta correcto
+// Ojo: AL usar operadores de repeticion concidera el nongreedy primero
+// podras evitar bugs no intencionados
+
+/**************** Creando objetos RegExp Dinamicamente ********************/
+
+// Aveces no sabremos con exactitud el patron que quieres hacer match
+// Lo que podemos hacer es craer una varaible con el patron abuscar y
+// mediante una instancia a RegExp() -> le pasamos este patron
+
+let nombre = "harry"; // -> patron a buscar
+let texto = "Harry es el mejor mago";
+let regexp = new RegExp(`\\b(${nombre})\\b`, "gi"); // -> como no usamos / / para la expresion regular \b se usa como -> \\b
+// El contructor recive la exresion regular de busqueda y los paramtros de busqueda
+// en este caso g-> busqueda globa, i-> insesitive Case (no le imporata las mayusculas o minusculas)
+// Usemos texto -> para hacer match con nombre y agregarle _nombre_
+console.log(texto.replace(regexp, "_$1_"));
+// > _Harry_ es el mejor mago
+nombre = "mago";
+console.log(texto.replace(regexp, "_$1_"));
+
+/************ IndexOf() vs search() *********/
+
+// no podemos usar Expresiones regulares en IndexOf, pero podemos usar search
+// search, retornara -1 si no ahi match o el indice del primer match
+
+console.log("   world".search(/\S/)); // -> busca caracter que no sea espacio
+// > 3 -> contando de 0 a 3, la cuarta posicion hace el primer match
+
+console.log("   ".search(/\S/)); // -> busca caracter que no sea espacio
+// > -1 no encuentra el match
+
+// source method
+
+let regx = /\d+/;
+console.log(regx.source); // -> retorna la expresion regular como string
+// > \d+
+
+// lastIndex
+// el metodo search no tiene la carateristica de IndexOf
+// poder tener un punto de partida de busqueda IndesOf("a",2)-> busca desde la posicion 2 en adelante
+// pero exiate lastIndex, el cual nos da una posicion del cual empezar a buscar
+let patron = /y/g;
+patron.lastIndex = 3; // busca el match desde el index 3 en adelante del string
+let match = patron.exec("xyzzy");
+console.log(match.index);
+// > 4
+console.log(patron.lastIndex);
+// >5
+// Por defecto lastIndex toma la posicion siguiente al ultimo match
+// en caso de que no se encuentre un match lastIndex tomar 0 (cero)
+
+/* Sticky*/
+let global = /abc/g;
+console.log(global.exec("xyz abc"));
+// >["abc"]
+let sticky = /abc/y;
+// sticky aceptara el match solo cuando el lastIndex este en la posicion del patron
+console.log(sticky.lastIndex); // -> 0
+// esto esta apuntando a "x" -> encuentra x, por lo cual no hace match con abc
+console.log(sticky.exec("xyz abc"));
+// > null
+// Como hariamos que funcione?
+// cambiemos el lasstIndex manualmente
+sticky.lastIndex = 4;
+console.log(sticky.exec("xyz abc"));
+// > [ 'abc', index: 4, input: 'xyz abc', groups: undefined ]
+
+// OJO, si usamos varias veces exec() en un patron global, esto actualiza automaticamnte
+// el lastIndex, por lo cual la busqueda puede generar errores
+let num = /\d+/g;
+console.log(num.exec("aqui el num: 1"));
+// >[ '1', index: 13, input: 'aqui el num: 1', groups: undefined ]
+console.log(num.exec("y ahora: 2"));
+// null
+
+// Tener cuidado cuando se usa un patron de manera global
+
+/***************  match() method *************************/
+
+//cuando definimos un patron de manera global, el match retornara un array con  todos las coincidencias
+console.log("banana".match(/an/g));
+// > ["an", "an"]
+
+/********** Bucles para todos los matchs *****************/
+
+// podemos aprovechar el comportamiento de un patron glogal
+// mediante el uso del lastIndex
+
+let input = "A string with 3 numbers in it ... 42 and 88";
+let number = /\d+/g;
+let match;
+while ((match = number.exec(input))) {
+  console.log(`Numero encontrado: ${match[0]} en ${match.index}`);
+}
+// > Numero encontrado: 3 en 14
+// > Numero encontrado: 42 en 34
+// > Numero encontrado: 88 en 41
+
+// vemos que el lastIndex se ira incrementando hasta que llegue el momento que sobre pase
+// el tamaño del string, en ese caso retornara null y terminara el bucle
+// y lastIndex se pondra en 0 cero
+
+/********* split with Regex ***************/
+
+let word = "Hola'1'a'3'todos'6'como'7'estan";
+console.log(word.split(/\'\d+\'/));
+//> [ 'Hola', 'a', 'todos', 'como', 'estan' ]
